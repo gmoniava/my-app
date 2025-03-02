6a5cf0e06a017e0080549ac59d7906157f15f583
gmoniava <moniava.g@gmail.com>
2025-03-02 23:45:26 +0400

    add some CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,17 @@ import Link from "next/link";
 
 export default async function Page() {
   return (
-    <div>
-      Welcome
-      <div>
-        Go to{" "}
-        <Link className="btn-primary" href="/main">
-          Main
-        </Link>
-        <Link className="btn-primary" href="/login">
-          Login
-        </Link>
+    <div className="flex justify-center items-center h-full">
+      <div className=" w-1/3 border border-slate-300 p-[10px] h-[620px] rounded-md flex flex-col">
+        <div className="text-2xl flex-1 text-center">Welcome to Movie App</div>
+        <div className="flex justify-around">
+          <Link className="btn-primary" href="/main">
+            Main
+          </Link>
+          <Link className="btn-primary" href="/login">
+            Login
+          </Link>
+        </div>
       </div>
     </div>
   );
