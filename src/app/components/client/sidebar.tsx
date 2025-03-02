3fbfffe9bbb736fa2b67e3c0dc7d9dc958d68cb9
gmoniava <moniava.g@gmail.com>
2025-03-02 22:24:05 +0400

    make layout client component

--- src/app/components/client/sidebar.tsx
+++ src/app/components/client/sidebar.tsx
@@ -2,11 +2,23 @@
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import { logout } from "../../lib/auth";
+import { useTransition } from "react";
 
 export default function Page(props: any) {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <div className="border-r h-full w-[300px]">
-      <button className={"btn-primary"} type="submit" onClick={props.logout}>
+      <button
+        className={"btn-primary"}
+        type="submit"
+        onClick={() => {
+          startTransition(async () => {
+            await logout();
+            redirect("/");
+          });
+        }}
+      >
         Logout
       </button>
     </div>
--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,24 +1,33 @@
+"use client";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Sidebar from "@/app/components/client/sidebar";
 import { logout } from "@/app/lib/auth";
 import { redirect } from "next/navigation";
+import React from "react";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  let [showSidebar, setShowSidebar] = React.useState(false);
   return (
-    <div lang="en" className="h-full flex">
-      <Sidebar
-        logout={async () => {
-          "use server";
-          await logout();
-          redirect("/");
-        }}
-      />
-      <div>{children}</div>
+    <div lang="en" className="h-full flex ">
+      {showSidebar && <Sidebar />}
+      <div className="flex-1">
+        <div className="border-b">
+          <div
+            className="btn-primary"
+            onClick={() => {
+              setShowSidebar(!showSidebar);
+            }}
+          >
+            Toggle sidebar
+          </div>
+        </div>
+        {children}
+      </div>
     </div>
   );
 }
