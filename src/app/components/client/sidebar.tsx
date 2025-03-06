49ccfb02492ad128356d1f02639e09a5d3438e7f
gmoniava <moniava.g@gmail.com>
2025-03-07 00:25:37 +0400

    use zod, some style changes

--- src/app/components/client/sidebar.tsx
+++ src/app/components/client/sidebar.tsx
@@ -2,24 +2,44 @@
 import { redirect } from "next/navigation";
 import { logout } from "../../lib/auth";
 import { useTransition } from "react";
+import Link from "next/link";
 
 export default function Page(props: any) {
   const [isPending, startTransition] = useTransition();
 
   return (
-    <div className="border-r h-full w-[300px]">
-      <button
-        className={"btn-primary"}
-        type="submit"
-        onClick={() => {
-          startTransition(async () => {
-            await logout();
-            redirect("/");
-          });
-        }}
-      >
-        Logout
-      </button>
+    <div className="border-r h-full w-[300px] flex flex-col gap-1 items-center">
+      <div>
+        {" "}
+        <Link href="/main/add-movie">
+          <button className="cursor-pointer" type="submit">
+            Add movie
+          </button>
+        </Link>
+      </div>
+
+      <div>
+        <Link href="/main/movies">
+          <button className="cursor-pointer" type="submit">
+            Search movies
+          </button>
+        </Link>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="cursor-pointer"
+          onClick={() => {
+            startTransition(async () => {
+              await logout();
+              redirect("/");
+            });
+          }}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,21 +1,28 @@
 "use server";
 import postgres from "postgres";
 import { NextResponse } from "next/server"; // To send a response
+import { z } from "zod";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
+const FormSchema = z.object({
+  name: z.string(),
+  release_year: z.coerce.number(),
+  actors: z.string(),
+  description: z.string(),
+  genres: z.array(z.coerce.number()),
+});
+
 export async function addMovie(formData: FormData) {
-  const name = formData.get("name") as string;
-  const release_year = parseInt(formData.get("release_year") as string);
-  const actors = formData.get("actors") as string;
-  const description = formData.get("description") as string;
-  const genres = formData.getAll("genres") as string[]; // Array of selected genre IDs
-
-  if (!name || !release_year || !actors || !description || genres.length === 0) {
-    throw new Error("Missing required form data");
-  }
-
   try {
+    const { name, release_year, actors, description, genres } = FormSchema.parse({
+      name: formData.get("name"),
+      release_year: formData.get("release_year"),
+      actors: formData.get("actors"),
+      description: formData.get("description"),
+      genres: formData.getAll("genres"),
+    });
+
     // Start a safe transaction
     await sql.begin(async (tx) => {
       // Insert movie into movies table
