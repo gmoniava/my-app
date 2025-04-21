8e0cf86d2d2f1906fd72e7f4462565ff319bc6b2
gmoniava <moniava.g@gmail.com>
2025-04-21 23:03:14 +0400

    experimenting with images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
-
+import moviesPng from "./images/movies.png";
 export default async function Page() {
   return (
     <div className="flex justify-center items-center h-full">
       <div className=" w-1/3 border border-slate-300 p-[10px] h-1/3 rounded-md flex flex-col text-center">
-        <div className="flex-3 flex flex-col items-center gap-5">
+        <div className="flex-3">
           <div className="text-2xl">Welcome to the Movie App</div>
           <div className="mt-1">Welcome to the movie application. </div>
-          <Image
-            src={"/images/movies.png"}
-            alt="Picture of the author"
-            width={100}
-            height={100}
-            className="mt-1 mb-1"
-          />
+          <Image src={moviesPng} alt="Picture of the author" className="mt-1 mb-1" />
         </div>
-        <div className="flex-1 flex justify-center items-start ">
+        <div className="flex justify-center items-start ">
           <Link className="btn-primary inline-block" href="/login">
             Login
           </Link>
