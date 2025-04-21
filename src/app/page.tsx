ea70ad0a2acaf55531afa6ea7c8e52f1abaeffce
gmoniava <moniava.g@gmail.com>
2025-04-21 23:06:27 +0400

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default async function Page() {
   return (
     <div className="flex justify-center items-center h-full">
       <div className=" w-1/3 border border-slate-300 p-[10px] h-1/3 rounded-md flex flex-col text-center">
-        <div className="flex-3">
+        <div className="flex-2">
           <div className="text-2xl">Welcome to the Movie App</div>
           <div className="mt-1">Welcome to the movie application. </div>
-          <Image src={moviesPng} alt="Picture of the author" className="mt-1 mb-1" />
+          <Image src={moviesPng} alt="Picture of the author" width={100} height={100} className="mt-1 mb-1 mx-auto" />
         </div>
-        <div className="flex justify-center items-start ">
+        <div className="flex-1 flex justify-center items-start ">
           <Link className="btn-primary inline-block" href="/login">
             Login
           </Link>
